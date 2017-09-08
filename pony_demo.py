10a27de8b34e9464eb8cea8645b7dc238e14fd30
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-09-08 18:30:45 +0300

    Update and rename pony_sample.py to pony_demo.py

--- pony_demo.py
+++ pony_demo.py
@@ -2,6 +2,7 @@
 """Prototype for central database in PonyORM."""
 
 from pony import orm
+import pandas as pd
 
 db = orm.Database()
 db.bind(provider='sqlite', filename=':memory:')
@@ -11,17 +12,14 @@ class Datapoint(db.Entity):
      name = orm.Required(str)     
      date = orm.Required(str)
      value = orm.Required(float)  
-     # TODO: make unique key freq, name, date
-        
+     # TODO: make unique key freq, name, date        
      
 db.generate_mapping(create_tables=True)
 
-orm.sql_debug(True)
+orm.sql_debug(False)
 # end of boilerplate
 
-# 0. Example setup 
-# this is to minic state of database before parser import 
-
+# Example setup - minics state of database before parser import 
 with orm.db_session:
      x = Datapoint(date="2014-03-31", freq='q', name="CPI_rog", value=102.3)
      # this datapoint will overlap:
@@ -66,6 +64,7 @@ def mock_parser_output_1():
         "name": "RUR_EUR_eop",
         "value": 79.7}
     
+    
 # put parcer data inside the database
 with orm.db_session:
     for mock_output in [mock_parser_output_1, mock_parser_output_2]: 
@@ -75,44 +74,49 @@ with orm.db_session:
 
 # 2. Query database
 # -----------------
-
  
 user_query = dict(varnames=['CPI_rog', 'RUR_EUR_eop'],  freq='m')
+print("\nQuery:", user_query)
 
-# TODO: make json, readable by pandas.read_json()
+@orm.db_session
+def query_db(user_query):
+    sel = orm.select((p.name, p.date, p.value) for p in Datapoint
+                     if p.name in user_query['varnames'] and
+                        p.freq==user_query['freq'])
 
-import pandas as pd
+    def as_dict(name, date, value):
+        return dict(name=name, date=date, value=value)  
 
-# Pseudocode:
-    
-# TODO: make filter for select() based on user_query 
-# TODO: get records
+    for tup in sel:
+        yield(as_dict(*tup))
 
-# make dataframe from records, similar to src.csv2df.emitter: 
-df = pd.DataFrame([{"date": "2015-12-31",
-"freq": "m",
-"name": "CPI_rog",
-"value": 100.8},
-{"date": "2015-12-31",
-"freq": "m",
-"name": "RUR_EUR_eop",
-"value": 79.7}])
+print("\nRaw result:")
+for d in query_db(user_query):
+    print(d)
+
+# make dataframe from records
+df = pd.DataFrame(query_db(user_query))
 df.date = pd.to_datetime(df.date)        
 df = df.pivot(columns='name', values='value', index='date')
 
+# save to json  - using pandas is slow, but this quarantees proper format
 df_json = df.to_json()
+
+
+# 3. User result
+# --------------
+
+# user reads json
 user_df = pd.read_json(df_json)
 
-# user_df.to_dict('list')
-# Out[49]: {'CPI_rog': [100.8], 'RUR_EUR_eop': [79.7]}
-
-
-# After 
-# -----
-# Screen what is inside the database
-
+# json read properly
+ref_df = pd.DataFrame({'CPI_rog': [100.8, 100.8], 'RUR_EUR_eop': [70.39, 79.7]})
+ref_df.index =  pd.DatetimeIndex(['2015-11-30', '2015-12-31'])
+assert user_df.equals(ref_df)
+        
+        
+# Screen after 
 with orm.db_session:    
     res = orm.select((p.name, p.freq, p.date, p.value) for p in Datapoint)[:]
-    
-pprint(res)
+#pprint(res)
     
