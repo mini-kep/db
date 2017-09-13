a54d35aa82873817b28972cfd9c4d23c75920220
varnie29a <varnie29a@lightsoft.ru>
2017-09-13 17:55:58 +0500

    #2 (unit tests for SQLAlchemy database operations). working on code review notices

--- tests/test_sqla_demo.py
+++ tests/test_sqla_demo.py
@@ -146,6 +146,68 @@ class Test_Read(FilledDatabase):
             .count()
         assert count == 1
 
+
+class Test_Insert(FilledDatabase):
+
+    def setUp(self):
+        super(Test_Insert, self).setUp()
+        self.non_existed_datapoint_values = dict(date="2017-07-10", freq='q', name="CPI_rog", value=77.7)
+
+    def get_non_existed_datapoint_condition(self):
+        return {k: v for k, v in self.non_existed_datapoint_values.items() if k != "value"}
+
+    def test_database_has_no_new_datapoint_which_has_not_been_inserted_yet(self):
+
+        condition = self.get_non_existed_datapoint_condition()
+
+        count = self.session.query(Datapoint) \
+            .filter_by(**condition) \
+            .count()
+        assert count == 0
+
+    def test_database_has_new_datapoint_after_it_has_been_inserted(self):
+
+        # tests that insert of new unique row actually works
+        new_datapoint = Datapoint(**self.non_existed_datapoint_values)
+        self.session.add(new_datapoint)
+        self.session.commit()
+        self.session.close()
+
+        condition = self.get_non_existed_datapoint_condition()
+
+        count = self.session.query(Datapoint) \
+            .filter_by(**condition) \
+            .count()
+        assert count == 1
+
+    def test_database_has_new_datapoint_with_proper_value_after_it_has_been_inserted(self):
+
+        # tests trhat insert of new unique row actually works (value testing)
+        new_datapoint = Datapoint(**self.non_existed_datapoint_values)
+        self.session.add(new_datapoint)
+        self.session.commit()
+        self.session.close()
+
+        condition = self.get_non_existed_datapoint_condition()
+
+        result = self.session.query(Datapoint) \
+            .filter_by(**condition) \
+            .one()
+        assert result.value == self.non_existed_datapoint_values["value"]
+
+    def test_duplicates_not_allowed_on_insert(self):
+
+        # this datapoint already exists in the Database
+        datapoint = Datapoint(**self.datapoint1_values)
+
+        with self.assertRaises(Exception):
+            self.session.add(datapoint)
+            self.session.commit()
+            self.session.close()
+
+        self.session.rollback()
+
+
 #TODO - CRITICAL: add combined primary key [freq-varname-date]
 
 # TODO: run tests from main
