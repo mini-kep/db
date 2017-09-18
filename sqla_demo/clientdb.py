27f442e2619a7a751c20cc48967bd015634a0c5e
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-09-18 11:59:03 +0300

    Add files via upload

--- sqla_demo/clientdb.py
+++ sqla_demo/clientdb.py
@@ -1,12 +1,16 @@
-"""Access functions to start database in SQLa and read/insert/update/delete rows.
-
-SQLa = sqlalchemy
+"""Access functions to start/kill database in SQLalchemy and 
+   and read/insert/update/delete rows.
 """
+
+# TODO FIXME: needs better session manager, repeated code in access classes read/update
+# TODO NEW: must have explicit upsert scenario 
+# TO CONSIDER: conceptiually still mixed (date-freq-name key + value) operation 
+#              and datapoint operation 
+
 
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-#FIXME: do we need to import Base from Datapoint?
-from sqla_demo.datapoint import Datapoint, Base
+from datapoint import Datapoint, Base
 
 
 def create_engine(engine_url='sqlite://'):
@@ -27,6 +31,34 @@ def drop_tables(engine):
 def create_session_factory(engine):
     """Return a function, which is used to start a session.""" 
     return sessionmaker(bind=engine)
+
+#TODO: enable session context manager
+#      see usage in PonyORM in this repo
+#      option 1: as in code below
+#      option 2: with explicit __enter__ and __exit__ methods
+
+#from contextlib import contextmanager
+#
+#@contextmanager
+#def session_scope():
+#    """Provide a transactional scope around a series of operations."""
+#    session = Session()
+#    try:
+#        yield session
+#        session.commit()
+#    except:
+#        session.rollback()
+#        raise
+#    finally:
+#        session.close()
+#
+#
+#def run_my_program():
+#    with session_scope() as session:
+#        ThingOne().go(session)
+#        ThingTwo().go(session)
+        
+
 
 # session / row operations
 
@@ -92,8 +124,8 @@ def find_by(session_factory, condition=None):
     finally:
         session.close()
 
-# FIXME: this should be explicit dictionary, not reverse reading from datapoint
-def get_datapoint_condition(datapoint):
+# FIXME: may delete
+def strip_value(datapoint):
     return {k: v for k, v in datapoint.items() if k != "value"}
 
 
@@ -103,18 +135,35 @@ if __name__ == '__main__':
     create_tables(engine)
     session_factory = create_session_factory(engine)
 
-    dp1_raw = dict(date="2014-03-31", freq='q', name="CPI_rog", value=102.3)
-    result_of_insert = insert_one(session_factory, Datapoint(**dp1_raw))
+    #sample data for datapoints
+    d1 = dict(date="2014-03-31", freq='q', name="CPI_rog", value=102.3)
+    d1_bis = dict(date="2014-03-31", freq='q', name="CPI_rog", value=102.0)
+    d2 = dict(freq='m', name='BRENT', date='2017-09-20', value=50.25)
+    
+    # insert some non-existing datapoints
+    is_inserted_1 = insert_one(session_factory, Datapoint(**d1))
+    is_inserted_2 = insert_one(session_factory, Datapoint(**d2))
+    assert (is_inserted_1 and is_inserted_2)
 
-    dp1_raw_with_updated_value = {**dp1_raw}
-    condition = get_datapoint_condition(dp1_raw_with_updated_value)
-    result_of_find_by = find_by(session_factory, condition)
+    # find a datapoint by date-freq-name    
+    condition = dict(date="2014-03-31", freq='q', name="CPI_rog")
+    found = find_by(session_factory, condition)
+    assert isinstance(found, list)
+    assert found[0].id > 0
+    found[0].id = None
+    assert found[0] == Datapoint(freq='q', name='CPI_rog', 
+                                 date='2014-03-31', value=102.3)
+    
+    # update with value
+    condition = dict(date="2014-03-31", freq='q', name="CPI_rog")
+    is_updated = update_one(session_factory, condition, 102.0)
+    assert is_updated
 
-    condition = get_datapoint_condition(dp1_raw_with_updated_value)
-    result_of_update_one = update_one(session_factory, condition, dp1_raw_with_updated_value["value"])
+    # find a datapoint by date-freq-name
+    condition = strip_value(d2)
+    is_deleted = delete_one(session_factory, condition)
+    assert is_deleted
+    found2 = find_by(session_factory)
+    assert isinstance(found2[0], Datapoint)
 
-    condition = get_datapoint_condition(dp1_raw)
-    result_of_delete_one = delete_one(session_factory, condition)
-    result_of_find_by = find_by(session_factory)
-
-    drop_tables(engine)
+    drop_tables(engine)   
--- sqla_demo/datapoint.py
+++ sqla_demo/datapoint.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-
 from sqlalchemy import Column, Integer, String, Float, UniqueConstraint
-
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -9,6 +6,10 @@ Base = declarative_base()
 class Datapoint(Base):
 
     __tablename__ = 'datapoints'
+    
+    __table_args__ = (
+        UniqueConstraint("freq", "name", "date"),
+    )
 
     id = Column(Integer, nullable=False, 
                          unique=True, 
@@ -19,32 +20,42 @@ class Datapoint(Base):
     
     # FIXME: maybe date column should have Date type? 
     #        how the conversion is made?
-    #        is it ok to for incoming json to be YYYY-MM-DD string?
+    #        is it ok for incoming json to be YYYY-MM-DD string?
     date = Column(String, nullable=False)
-    # end
+    # issue end ---------------------------------------
     
-    value = Column(Float, nullable=False)
+    value = Column(Float, nullable=False)    
 
-    __table_args__ = (
-        UniqueConstraint("freq", "name", "date"),
-    )
+    def __eq__(self, x):   
+        return all([self.id == x.id, 
+                    self.freq == x.freq,
+                    self.name == x.name,                   
+                    self.date == x.date,
+                    self.value == x.value])
 
-    def __repr__(self):        
-        args = (('freq', self.freq), 
-                ('name', self.name),
-                ('date', self.date), 
+    def __repr__(self):   
+        args = (('id', self.id),
+                ('freq', quoted(self.freq)), 
+                ('name', quoted(self.name)),
+                ('date', quoted(self.date)), 
                 ('value', self.value))
-        args_str = [f'{k}={v}' for k, v in args] 
+        args_str = [f"{k}={v}" for k, v in args] 
         return "Datapoint({})".format(", ".join(args_str ))
     
-# NOT TODO: there can be a second table with additional information 
-#          (parser name, timestamp, inserted)
+def quoted(s):
+    s = str(s)
+    return f"'{s}'" 
+
 
 if __name__=='__main__':
-    d = Datapoint(freq='m', name='BRENT', date='2017-09-20', value=50.25)
+    x = dict(freq='m', name='BRENT', date='2017-09-20', value=50.25)
+    d = Datapoint(**x)
+    # testing __e__q method
+    assert d == d
     assert isinstance(d.freq, str)
     assert isinstance(d.name, str)
     assert isinstance(d.value, float)
-    # likely fails
-    assert isinstance(d.date, str)    
-    
+    # likely fails after date type change
+    assert isinstance(d.date, str) 
+    # id not assigned before insert
+    assert d.id is None
