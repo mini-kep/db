6e5c9c57d494af546a95239e95722f6f15b4c49c
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-09-18 15:35:43 +0300

    docstring edited

--- clientdb.py
+++ clientdb.py
@@ -1,5 +1,11 @@
-"""Access functions to start/kill database in SQLalchemy and 
-   and read/insert/update/delete rows.
+"""Pure SQLAlchemy implementation of CRUD methods to:
+   - start/kill database 
+   - read/insert/update/delete rows.
+   
+Intent: make test scenarios applicable to production implementation either 
+        as <https://docs.djangoproject.com/en/1.11/topics/db/models/>
+        or <http://flask-sqlalchemy.pocoo.org/2.1/>.
+  
 """
 
 # TODO FIXME: needs better session manager, repeated code in access classes read/update
