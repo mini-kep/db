2674fa45454ab0d868497cfad0d8482653900543
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-10-03 12:28:02 +0300

    Update requests_tests.py

--- requests_tests.py
+++ requests_tests.py
@@ -1,8 +1,9 @@
 import requests
 
-BASE_URI = 'api/datapoints'
 POST_URI = 'api/incoming'
+BASE_URI = 'api/datapoints'
 
+# for reference
 # data = [{
     # 'name': 'BRENT',
     # 'freq': 'm',
@@ -10,48 +11,56 @@ POST_URI = 'api/incoming'
     # 'value': 42.0
 # }]
 
-payload = {'name': 'BRENT', 'freq': 'm'}
-    
 
-class TestGET():
+#TODO (EP): must change this to actual data, not imaginary, both here and in SPEC.md
+
+class TestPOST():
+    # success
+    def test_posting_valid_data():
+        """Return an empty JSON on success"""
+        data = [{ "date": "1999-03-31", "freq": "q", "name": "INVESTMENT_bln_rub", "value": 12345.6 }]
+        response = requests.post(POST_URI, data=data)
+        assert response.json() == {}
+
+    # fail    
+    def test_posting_data_with_empty_name_field():
+        """Results in a 400 error"""
+        data = [{ "date": "1999-03-31", "freq": "q", "name": "", "value": 12345.6 }]
+        response = requests.post(POST_URI, data=data)
+        assert response.status_code == 400
+
+
+# parameter for GET method
+payload = {'name': 'BRENT', 'freq': 'd'}   
+
+
+class Test_GET():
+    # happy paths
     def test_request_response_with_valid_params():
-        """Should complete successfully with OK status code"""
+        """Completed successfully with OK status code"""
         response = requests.get(BASE_URI, params=payload)
         assert response.ok
 
     def test_getting_BRENT():
-        """Should return a list with items that have the requested variable"""
+        """Return a list with items that have the requested variable"""
         response = requests.get(BASE_URI, params=payload)
         assert response.json()[0]['name'] == 'BRENT'
 
+    # sad paths
     def test_request_with_empty_name():
-        """Should return 400 error with empty name parameter"""
+        """Return 400 error with empty name parameter"""
         payload = {'name': '', 'freq': 'm'}
         response = requests.get(BASE_URI, params=payload)
         assert response.status_code == 400
 
     def test_request_with_empty_frequency():
-        """Should return 400 error with empty freq parameter"""
+        """Return 400 error with empty freq parameter"""
         payload = {'name': 'BRENT', 'freq': ''}
         response = requests.get(BASE_URI, params=payload)
         assert response.status_code == 400
 
     def test_getting_data_not_found():
-        """Should return response with empty json"""
+        """Return response with empty json"""
         payload = {'name': 'FOOBAR', 'freq': 'm'}
         response = requests.get(BASE_URI, params=payload)
         assert response.json() == {}
-
-
-class TestPOST():
-    def test_posting_valid_data():
-        """Should return an empty JSON on success"""
-        data = [{ "date": "1999-03-31", "freq": "q", "name": "INVESTMENT_bln_rub", "value": 12345.6 }]
-        response = requests.post(POST_URI, data=data)
-        assert response.json() == {}
-
-    def test_posting_data_with_empty_name_field():
-        """Should result in a 400 error"""
-        data = [{ "date": "1999-03-31", "freq": "q", "name": "", "value": 12345.6 }]
-        response = requests.post(POST_URI, data=data)
-        assert response.status_code == 400
