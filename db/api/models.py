c1854abe0e90eb5422b69289518927d547b8fae1
SuperVasya <mcjack@inbox.ru>
2017-10-05 13:06:40 +0300

    Added Unique Constraitn on freq-name-date Added travis file

--- db/api/models.py
+++ db/api/models.py
@@ -3,24 +3,15 @@ from db import db
 
 
 class Datapoint(db.Model):
+    __table_args__ = (
+        db.UniqueConstraint("freq", "name", "date"),
+    )
+
     id = db.Column(db.Integer,primary_key=True)
-    # COMMENT (EP): for id we had a lot of properties, not sure about all, but we are liekly to need 'autoincrement=True'  
-    
-    #    id = Column(Integer, nullable=False, 
-    #                     unique=True, 
-    #                     autoincrement=True, 
-    #                      primary_key=True) 
-    
     freq = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     date = db.Column(db.Date, nullable=False)
     value =db.Column(db.Float, nullable=False)
-
-    # COMMENT (EP): I think we will need a <freq-name-date> composite key in order to work with existing vs new values in the database 
-    # from https://github.com/mini-kep/db/blob/master/demo/sqlalchemy/datapoint.py#L10-L12 :
-    #    __table_args__ = (
-    #             UniqueConstraint("freq", "name", "date"),
-    #              )
     
     @property
     def serialize(self): # Add serialize method for jsonify
@@ -29,5 +20,4 @@ class Datapoint(db.Model):
            'name': self.name,
            'date': datetime.strftime(self.date, "%Y-%m-%d"),
            'value':self.value
-       }   
-    
+       }
