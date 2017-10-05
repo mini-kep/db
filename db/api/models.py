dbf4e0286958a3731cc51e7294314a5f8cccc696
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-10-05 12:06:48 +0300

    Update models.py

--- db/api/models.py
+++ db/api/models.py
@@ -4,12 +4,25 @@ from db import db
 
 class Datapoint(db.Model):
     id = db.Column(db.Integer,primary_key=True)
+    # COMMENT (EP): for id we had a lot of properties, not sure abnout all, but we are liekly to need 'autoincrement=True'  
+    
+    #    id = Column(Integer, nullable=False, 
+    #                     unique=True, 
+    #                     autoincrement=True, 
+    #                      primary_key=True) 
+    
+    
+    
     freq = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     date = db.Column(db.Date, nullable=False)
     value =db.Column(db.Float, nullable=False)
 
     # COMMENT (EP): I think we will need a <freq-name-date> composite key in order to work with existing vs new values in the database 
+    # from https://github.com/mini-kep/db/blob/master/demo/sqlalchemy/datapoint.py#L10-L12 :
+    #    __table_args__ = (
+    #             UniqueConstraint("freq", "name", "date"),
+    #              )
     
     @property
     def serialize(self): # Add serialize method for jsonify
