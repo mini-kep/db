4cb01ccee06813877d2d87faf52b81f13bf7aa33
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-10-06 00:04:26 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,10 +5,13 @@ from db import app, db
 from db.api.views import api as api_module
 from config import basedir
 
+# FIXME (EP):  move tests.py and test_josn to /tests folder?
+
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        app = Flask(__name__)
+        app = Flask(__name__)        
+        # FIXME (EP): maybe better have a separate test config?        
         app.config.from_object('config.DevelopmentConfig')
         app.register_blueprint(api_module)
         db.create_all()
@@ -26,16 +29,17 @@ class TestCase(unittest.TestCase):
                                  data=data,
                                  headers=dict(API_TOKEN=app.config['API_TOKEN']))
         assert response.status_code == 200
-
+        
+    # FIXME: I suggest the test name to be extended to reflect if it a good or bad result   
     def test_auth(self):
         response = self.app.post('/api/incoming')
         assert response.status_code == 403
 
+    # FIXME: maybe also check for some values, not jeust response code?    
     def test_get_request(self):
         params = dict(name='BRENT', freq='d')
         response = self.app.get('/api/datapoints', query_string=params)
         assert response.status_code == 200
-
 
 if __name__ == '__main__':
     unittest.main()
