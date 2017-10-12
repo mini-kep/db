4399d987ce9c17a5ab1562f2d1d850094db87572
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-10-12 12:39:42 +0300

    comments on tests for #10

--- tests/tests.py
+++ tests/tests.py
@@ -1,46 +1,55 @@
-import unittest
 import json
 import os
 from random import randint
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.sys.path.insert(0,parentdir)
+import unittest
+
 from db import create_app, db
 from db.api.views import api as api_module
 from utils import to_date
 
+# EP: must have repo root on path, but why exactly are we doing this? 
+#     I ran the tests without path tweaking and they worked locally
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir)
 
+# create test app
 app = create_app('config.TestingConfig')
 app.register_blueprint(api_module)
 
-
 class TestCase(unittest.TestCase):
 
+    def _read_test_data(self):
+        tests_folder = os.path.abspath(os.path.dirname(__file__))
+        with open(os.path.join(tests_folder,'test_data.json')) as data_file:
+            return data_file.read()    
+    
     def setUp(self):
         db.create_all(app=app)
-        self.app = app.test_client()
+        self.app = app.test_client()        
 
     def tearDown(self):
         db.session.remove()
         db.drop_all(app=app)
+
+class TestAPI_Incoming(TestCase):
 
     def test_auth_forbidden(self):
         response = self.app.post('/api/incoming')
         assert response.status_code == 403
 
     def test_upload_data(self):
-        tests_folder = os.path.abspath(os.path.dirname(__file__))
-        with open(os.path.join(tests_folder,'test_data.json')) as data_file:
-            data = data_file.read()
+        data = self._read_test_data()
         response = self.app.post('/api/incoming',
                                  data=data,
                                  headers=dict(API_TOKEN=app.config['API_TOKEN']))
         assert response.status_code == 200
 
+    # methods below may not pass  <https://github.com/mini-kep/intro/wiki/Testing-guidelines#checklist>  
+    
+    # FIXME: this should be two tests with one assert per test, may not pass  <https://github.com/mini-kep/intro/wiki/Testing-guidelines#checklist>     
     def test_json_output_format(self):
         # Upload data
-        tests_folder = os.path.abspath(os.path.dirname(__file__))
-        with open(os.path.join(tests_folder, 'test_data.json')) as data_file:
-            data = data_file.read()
+        data = self._read_test_data()
         self.app.post('/api/incoming',
                       data=data,
                       headers=dict(API_TOKEN=app.config['API_TOKEN']))
@@ -55,11 +64,10 @@ class TestCase(unittest.TestCase):
         assert response.status_code == 200
         assert response_body == expected_response
 
+    # FIXME: this is such a nested test, need refactor 
     def test_csv_output_format(self):
         # Upload data
-        tests_folder = os.path.abspath(os.path.dirname(__file__))
-        with open(os.path.join(tests_folder, 'test_data.json')) as data_file:
-            data = data_file.read()
+        data = self._read_test_data()
         self.app.post('/api/incoming',
                       data=data,
                       headers=dict(API_TOKEN=app.config['API_TOKEN']))
@@ -74,7 +82,8 @@ class TestCase(unittest.TestCase):
         random_data_to_check = f'{random_data_point["date"]},{random_data_point["value"]}'
         # Check if random datapoint exists in csv
         assert random_data_to_check in response_body.split('\n')
-
+        
+    # TODO: we do not have tests for getting datapoints yet 
 
 if __name__ == '__main__':
     unittest.main()
