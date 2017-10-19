878229ee6e036fa1c5d28bc2fe8377d765545780
=epogrebnyak <e.pogrebnyak@gmail.com>
2017-10-19 08:05:50 +0300

    new base class

--- tests2/test_basic.py
+++ tests2/test_basic.py
@@ -1,6 +1,19 @@
+import json
 import unittest
+import os
+
 from flask import current_app
+
 from db import _app
+from db import db as fsa_db
+from db.api import utils  
+from db.api.models import Datapoint
+
+def read_test_data(filename = 'test_data_2016H2.json'):
+    tests_folder = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(tests_folder, filename)
+    with open(path) as file:
+        return json.load(file)
 
 # db/__init__.py
 
@@ -17,8 +30,7 @@ from db import _app
 #        app.config.from_object(config)
 #    db.init_app(app)
 #    return app
-
-from db import db as fsa_db
+    
 
 def make_app():
     app = _app() # was: Flask(__name__)
@@ -27,7 +39,14 @@ def make_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False        
     return app 
 
+
 class BasicsTestCase(unittest.TestCase):
+    def prepare_db(self):
+        data = read_test_data()
+        for datapoint in data:
+            datapoint['date'] = utils.to_date(datapoint['date'])
+        fsa_db.session.bulk_insert_mappings(Datapoint, data)
+    
     def setUp(self):
         self.app = make_app()               
         self.app_context = self.app.app_context()
@@ -46,3 +65,4 @@ class BasicsTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(module='test_basic')
+    z = read_test_data()
