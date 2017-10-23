c61fe92fbfea511378f53d812faf296b3ba19bba
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-10-23 17:11:30 +0300

    fixme for #9

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from db import create_app
 from db.api.views import api as api_module
+# FIXME: naming has to be symmetric - why not 'db.custom_api.views'?
 from db.custom_api.custom_api_blueprint import custom_api_bp as custom_api_module
 
 
