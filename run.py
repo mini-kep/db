742bd33b440f80c17dc6d3f2daa90383738e717b
mwangikinuthia <mwangikinuthia@hotmail.com>
2017-11-09 02:27:56 +0300

    Swagger doc with flasgger

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from db import create_app
 from db.api.views import api as api_module
 from db.custom_api.views import custom_api_bp as custom_api_module
+from flasgger import Swagger
 
 
 if __name__ == '__main__':
     app = create_app('config.ProductionConfig')
     app.register_blueprint(api_module)
     app.register_blueprint(custom_api_module)
-    app.run(host="0.0.0.0", port=int(app.config['PORT']))
+    Swagger(app)
+    app.run(host="0.0.0.0")
