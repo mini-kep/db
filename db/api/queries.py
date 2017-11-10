ca77759066733ae6526c4372be9e13a8b755c351
Igaloidas <ignaskiela@super.lt>
2017-11-10 21:46:14 +0200

    Bugfix

--- db/api/queries.py
+++ db/api/queries.py
@@ -78,7 +78,9 @@ def delete(name=None, unit=None):
     """
     if name:
         Datapoint.query.filter(Datapoint.name.startswith(name)).delete(synchronize_session=False)
+        db.session.commit()
     elif unit:
         Datapoint.query.filter(Datapoint.name.startswith(unit)).delete(synchronize_session=False)
+        db.session.commit()
     else:
         raise ValueError("No parameters given")
