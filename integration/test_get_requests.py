88a0673931973add7b27c91f95c5cee01d8e8ba0
Vasily <vasmitr95@gmail.com>
2017-11-26 23:01:47 +0500

    dev-issue-68: Added integration tests for api

--- integration/test_get_requests.py
+++ integration/test_get_requests.py
@@ -1,151 +1,62 @@
-import requests
+from access import get_freq, get_names, get_df, get_info, get_datapoints, get_datapoints_full_response,\
+    get_ts, join_df, get_custom_series
 
 
-lists_of_name_by_freq = {
-    'a': [
-          "CPI_ALCOHOL_rog",
-          "CPI_FOOD_rog",
-          "CPI_NONFOOD_rog",
-          "CPI_rog",
-          "CPI_SERVICES_rog",
-          "EXPORT_GOODS_bln_usd",
-          "GDP_bln_rub",
-          "GDP_yoy",
-          "GOV_EXPENSE_ACCUM_CONSOLIDATED_bln_rub",
-          "GOV_EXPENSE_ACCUM_FEDERAL_bln_rub",
-          "GOV_EXPENSE_ACCUM_SUBFEDERAL_bln_rub",
-          "GOV_REVENUE_ACCUM_CONSOLIDATED_bln_rub",
-          "GOV_REVENUE_ACCUM_FEDERAL_bln_rub",
-          "GOV_REVENUE_ACCUM_SUBFEDERAL_bln_rub",
-          "GOV_SURPLUS_ACCUM_FEDERAL_bln_rub",
-          "GOV_SURPLUS_ACCUM_SUBFEDERAL_bln_rub",
-          "IMPORT_GOODS_bln_usd",
-          "INDPRO_yoy",
-          "INVESTMENT_bln_rub",
-          "INVESTMENT_yoy",
-          "RETAIL_SALES_bln_rub",
-          "RETAIL_SALES_FOOD_bln_rub",
-          "RETAIL_SALES_FOOD_yoy",
-          "RETAIL_SALES_NONFOOD_bln_rub",
-          "RETAIL_SALES_NONFOOD_yoy",
-          "RETAIL_SALES_yoy",
-          "TRANSPORT_FREIGHT_bln_tkm",
-          "UNEMPL_pct",
-          "WAGE_NOMINAL_rub",
-          "WAGE_REAL_yoy"
-    ],
-    'q': [
-      "CPI_ALCOHOL_rog",
-      "CPI_FOOD_rog",
-      "CPI_NONFOOD_rog",
-      "CPI_rog",
-      "CPI_SERVICES_rog",
-      "EXPORT_GOODS_bln_usd",
-      "GDP_bln_rub",
-      "GDP_yoy",
-      "GOV_EXPENSE_ACCUM_CONSOLIDATED_bln_rub",
-      "GOV_EXPENSE_ACCUM_FEDERAL_bln_rub",
-      "GOV_EXPENSE_ACCUM_SUBFEDERAL_bln_rub",
-      "GOV_REVENUE_ACCUM_CONSOLIDATED_bln_rub",
-      "GOV_REVENUE_ACCUM_FEDERAL_bln_rub",
-      "GOV_REVENUE_ACCUM_SUBFEDERAL_bln_rub",
-      "GOV_SURPLUS_ACCUM_FEDERAL_bln_rub",
-      "GOV_SURPLUS_ACCUM_SUBFEDERAL_bln_rub",
-      "IMPORT_GOODS_bln_usd",
-      "INDPRO_rog",
-      "INDPRO_yoy",
-      "INVESTMENT_bln_rub",
-      "INVESTMENT_rog",
-      "INVESTMENT_yoy",
-      "RETAIL_SALES_bln_rub",
-      "RETAIL_SALES_FOOD_bln_rub",
-      "RETAIL_SALES_FOOD_rog",
-      "RETAIL_SALES_FOOD_yoy",
-      "RETAIL_SALES_NONFOOD_bln_rub",
-      "RETAIL_SALES_NONFOOD_rog",
-      "RETAIL_SALES_NONFOOD_yoy",
-      "RETAIL_SALES_rog",
-      "RETAIL_SALES_yoy",
-      "TRANSPORT_FREIGHT_bln_tkm",
-      "UNEMPL_pct",
-      "WAGE_NOMINAL_rub",
-      "WAGE_REAL_rog",
-      "WAGE_REAL_yoy"
-    ],
-    'm': [
-      "CPI_ALCOHOL_rog",
-      "CPI_FOOD_rog",
-      "CPI_NONFOOD_rog",
-      "CPI_rog",
-      "CPI_SERVICES_rog",
-      "EXPORT_GOODS_bln_usd",
-      "GOV_EXPENSE_ACCUM_CONSOLIDATED_bln_rub",
-      "GOV_EXPENSE_ACCUM_FEDERAL_bln_rub",
-      "GOV_EXPENSE_ACCUM_SUBFEDERAL_bln_rub",
-      "GOV_REVENUE_ACCUM_CONSOLIDATED_bln_rub",
-      "GOV_REVENUE_ACCUM_FEDERAL_bln_rub",
-      "GOV_REVENUE_ACCUM_SUBFEDERAL_bln_rub",
-      "GOV_SURPLUS_ACCUM_FEDERAL_bln_rub",
-      "GOV_SURPLUS_ACCUM_SUBFEDERAL_bln_rub",
-      "IMPORT_GOODS_bln_usd",
-      "INDPRO_rog",
-      "INDPRO_yoy",
-      "INVESTMENT_bln_rub",
-      "INVESTMENT_rog",
-      "INVESTMENT_yoy",
-      "RETAIL_SALES_bln_rub",
-      "RETAIL_SALES_FOOD_bln_rub",
-      "RETAIL_SALES_FOOD_rog",
-      "RETAIL_SALES_FOOD_yoy",
-      "RETAIL_SALES_NONFOOD_bln_rub",
-      "RETAIL_SALES_NONFOOD_rog",
-      "RETAIL_SALES_NONFOOD_yoy",
-      "RETAIL_SALES_rog",
-      "RETAIL_SALES_yoy",
-      "TRANSPORT_FREIGHT_bln_tkm",
-      "UNEMPL_pct",
-      "WAGE_NOMINAL_rub",
-      "WAGE_REAL_rog",
-      "WAGE_REAL_yoy"
-    ],
-    'd': [
-      "BRENT",
-      "USDRUR_CB",
-      "UST_10YEAR",
-      "UST_1MONTH",
-      "UST_1YEAR",
-      "UST_20YEAR",
-      "UST_2YEAR",
-      "UST_30YEAR",
-      "UST_3MONTH",
-      "UST_3YEAR",
-      "UST_5YEAR",
-      "UST_6MONTH",
-      "UST_7YEAR"
-    ]
-}
+frequencies = {'a', 'q', 'm', 'd'}
 
-freqs = {'a', 'q', 'm', 'd'}
+if __name__ == '__main__':
+
+    #  АПИ возвращает ожидаемый сет частот
+    assert set(get_freq()) == frequencies
+
+    for freq in frequencies:
+        #  Получаем фрейм и список переменных для частоты
+        df = get_df(freq)
+        names = get_names(freq)
+
+        #  Список переменных фрейма идентичен списку переменных из АПИ
+        assert (names == df.keys()).any()
+
+        series = []
+        for name in names:
+
+            #  Получаем начальную и конечную дату серии для переменной
+            info = get_info(freq, name)
+            start_date = info[freq]['start_date']
+            latest_date = info[freq]['latest_date']
+
+            #  Получаем серию для переменной
+            series.append(get_ts(freq=freq, name=name, start_date=start_date, end_date=latest_date).to_frame())
+
+        #  Объединяем список серий в фрейм
+        joined_series = join_df(series)
+
+        # Фрейм идентичен фрейму полученному ранее
+        assert (joined_series == df).any().any()
+
+    # Неверная частота
+    response = get_datapoints_full_response(freq='z', name='CPI_roq')
+    assert response.status_code == 422
+    assert set(response.json()['allowed']) == frequencies
+
+    # Неверная переменная
+    response = get_datapoints_full_response(freq='q', name='ABC')
+    assert response.status_code == 422
+    assert response.json()['allowed'] == get_names('q')
+
+    # Начальная дата в будущем
+    response = get_datapoints_full_response(freq='d', name='BRENT', start_date='2025-01-01')
+    assert response.status_code == 422
+    assert isinstance(response.json(), dict)
+
+    # Начальная дата в больше конечной
+    response = get_datapoints_full_response(freq='d', name='BRENT', start_date='2015-01-01', end_date='2000-01-01')
+    assert response.status_code == 422
+    assert isinstance(response.json(), dict)
 
 
-def test_user_can_get_allowed_frequencies():
+    #  Кастомное АПИ возвращает то же, что и стандартное
+    standard_api_response = get_datapoints(freq='m', name='CPI_rog', start_date='2015-01-01', end_date='2017-12-31', format='csv')
+    custom_api_response = get_custom_series(freq='m', name='CPI', suffix='rog', start='2015', end='2017')
 
-    url = f'https://minikep-db.herokuapp.com/api/freq'
-
-    data = requests.get(url).json()
-
-    assert set(data) == freqs
-
-
-def test_user_can_get_names_by_frequency():
-
-    for freq in freqs:
-        url = f'https://minikep-db.herokuapp.com/api/names/{freq}'
-
-        data = requests.get(url).json()
-
-        assert data == lists_of_name_by_freq[freq]
-
-if __name__ == "__main__":
-    test_user_can_get_allowed_frequencies()
-    test_user_can_get_names_by_frequency()
+    assert (custom_api_response == standard_api_response).any()
