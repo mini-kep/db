46b28b85df7240db421088cf9a1c4cde72e67fea
ssin21 <kn3XjrvpT>
2017-12-03 21:06:33 +0200

    Adding tests for 'api/desc' endpoint. #92

--- tests/test_desc.py
+++ tests/test_desc.py
@@ -1,7 +1,32 @@
+"""
+# TODO: need tests for 'api/desc' post, get, delete methods using `requests` lib and base url for web endpoint
+
+
+#GET api/desc?abbr=GDP
+
+#Returns:
+#   {'abbr':'GDP', 'ru':'Цена нефти Brent', 'en':'Brent oil price'}
+
+#GET api/desc?abbr=rog
+
+#Returns:
+#    {unit:'rog', en:'rate of growth to previous period', ru='темп роста к пред. периоду'}
+
+
+#POST api/desc
+
+#Payload:
+#[dict(abbr='BRENT', ru='Цена нефти Brent', en='Brent oil price')
+#dict(abbr='GDP', ru='Валовый внутренний продукт', en='Gross domestic product')
+#dict(abbr='rog', ru='темп роста к пред. периоду', en='rate of growth to previous period')
+#dict(abbr='yoy', ru='темп роста за 12 месяцев', en='year-on-year rate of growth')]
+
+#DELETE api/desc?abbr=rog
+#DELETE api/desc?abbr=BRENT
+
+"""
 import pytest
 import requests
-
-from tests.test_basic import TestCaseBase
 
 DESC_URL = 'http://minikep-db.herokuapp.com/api/desc'
 
@@ -13,7 +38,7 @@ sample_post_payload = [
 ]
 
 
-class Test_ApiDesc(TestCaseBase):
+class Test_ApiDesc():
 
     def test_get_without_params_fails(self):
         response = requests.get(DESC_URL)
@@ -21,7 +46,7 @@ class Test_ApiDesc(TestCaseBase):
 
     @pytest.mark.xfail
     def test_get_GDP_ru_successful_returns_valid_data(self):
-        params = dict(abbr='GDP', lang='ru')
+        params = dict(abbr='GDP')
         response = requests.get(DESC_URL, params=params)
         data = response.json().decode('utf-8')
 
@@ -30,7 +55,7 @@ class Test_ApiDesc(TestCaseBase):
 
     @pytest.mark.xfail
     def test_get_rog_en_successful_returns_valid_data(self):
-        params = dict(abbr='GDP', lang='ru')
+        params = dict(abbr='GDP')
         response = requests.get(DESC_URL, params=params)
         data = response.json().decode('utf-8')
 
@@ -58,5 +83,5 @@ class Test_ApiDesc(TestCaseBase):
         assert response.status_code == 200
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # pragma no cover
     pytest.main([__file__])
