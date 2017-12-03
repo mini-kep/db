5f25000c8ffc33c3e1d774296c2e4a74a5532fdb
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-12-03 16:08:44 +0300

    Update desc.py

--- integration/desc.py
+++ integration/desc.py
@@ -1,25 +1,24 @@
 # TODO: need tests for 'api/desc' post, get, delete methods using `requests` lib and base url for web endpoint 
 
 
-#GET api/desc?head=GDP&lang=ru   
+#GET api/desc?abbr=GDP   
 
 #Returns:
-#   {'head':'GDP', 'ru':'Цена нефти Brent'}  on head
-#   All descriptions (head and unit) without parameter
+#   {'abbr':'GDP', 'ru':'Цена нефти Brent', 'en':'Brent oil price'}
 
-#GET api/desc?unit=rog&lang=en
+#GET api/desc?abbr=rog
 
 #Returns:
-#    {unit:'rog', en:'rate of growth to previous period'}
-#    All descriptions (head and unit) without parameter
+#    {unit:'rog', en:'rate of growth to previous period', ru='темп роста к пред. периоду'}
+
 
 #POST api/desc
 
 #Payload:
-#[dict(head='BRENT', ru='Цена нефти Brent', en='Brent oil price')
-#dict(head='GDP', ru='Валовый внутренний продукт', en='Gross domestic product')
-#dict(unit='rog', ru='темп роста к пред. периоду', en='rate of growth to previous period')
-#dict(unit='yoy', ru='темп роста за 12 месяцев', en='year-on-year rate of growth')]
+#[dict(abbr='BRENT', ru='Цена нефти Brent', en='Brent oil price')
+#dict(abbr='GDP', ru='Валовый внутренний продукт', en='Gross domestic product')
+#dict(abbr='rog', ru='темп роста к пред. периоду', en='rate of growth to previous period')
+#dict(abbr='yoy', ru='темп роста за 12 месяцев', en='year-on-year rate of growth')]
 
-#DELETE api/desc?unit=rog   
-#DELETE api/desc?head=BRENT
+#DELETE api/desc?abbr=rog   
+#DELETE api/desc?abbr=BRENT
