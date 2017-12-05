87039e3ce20bb873e5b718c855a18e5bd9da97e8
Alex <aarychagov@gmail.com>
2017-12-05 23:20:35 +0400

    #104: renaming

--- tests/test_views_series.py
+++ tests/test_views_series.py
@@ -27,20 +27,20 @@ sample_invalid_params_start_date_gt_end_date = dict(
 
 
 class Test_Api_Series(TestCaseBase):
-    def test_calling_without_params_should_fail(self):
+    def test_get_method_without_params_should_fail(self):
         response = self.client.get(API_SERIES_URL)
         assert response.status_code == BAD_PARAMETERS_STATUS_CODE
 
-    def test_calling_with_valid_params_is_ok(self):
+    def test_get_method_on_valid_params_is_ok(self):
         response = self.client.get(API_SERIES_URL, query_string=sample_valid_params)
         data = response.get_data().decode('utf-8')
         assert data == expected_output
 
-    def test_calling_with_valid_params_with_dates_is_ok(self):
+    def test_calling_on_valid_params_with_dates_is_ok(self):
         response = self.client.get(API_SERIES_URL, query_string=sample_valid_params_with_dates)
         data = response.get_data().decode('utf-8')
         assert data == expected_output_with_dates
 
-    def test_calling_with_invalid_params_start_date_gt_end_date_should_fail(self):
+    def test_calling_on_invalid_params_start_date_gt_end_date_should_fail(self):
         response = self.client.get(API_SERIES_URL, query_string=sample_invalid_params_start_date_gt_end_date)
         assert response.status_code == BAD_PARAMETERS_STATUS_CODE
