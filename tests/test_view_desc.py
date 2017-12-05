c61219dff8343bf30c94dc38c17405196d83ad87
ssin21 <kn3XjrvpT>
2017-12-05 22:08:52 +0200

    Updating tests for 'api/desc'. Using test_client(), updating naming. #92

--- tests/test_view_desc.py
+++ tests/test_view_desc.py
@@ -26,17 +26,15 @@ Tests for 'api/desc' post, get, delete methods
 
 """
 
-# TODO: 
-# - change to test_clinet(): use base test class
-# - rename tests: exclude variabnle names
-
 # NOT TODO:
 # - parametrise tests
 
 import pytest
-import requests
+from flask import json
 
-DESC_URL = 'http://minikep-db.herokuapp.com/api/desc'
+from tests.test_basic import TestCaseBase
+
+DESC_URL = 'api/desc'
 
 sample_post_payload = [
     dict(abbr='BRENT', ru='Цена нефти Brent', en='Brent oil price'),
@@ -46,60 +44,105 @@ sample_post_payload = [
 ]
 
 
-class Test_api_desc_GET_method():
+class Test_api_desc_GET_method(TestCaseBase):
 
     def test_get_method_without_params_fails(self):
-        response = requests.get(DESC_URL)
+        response = self.client.get(DESC_URL)
         assert response.status_code != 200
 
-        
-    # FIXME: split to two tests + rename without variable name   
+    def test_get_method_with_wrong_abbr_param_fails(self):
+        params = dict(abbr='wrong_params')
+        response = self.client.get(DESC_URL, query_string=params)
+        assert response.status_code != 200
+
     @pytest.mark.xfail
-    def test_get_GDP_ru_successful_returns_valid_data(self):
+    def test_get_on_valid_variable_successful_with_code_200(self):
         params = dict(abbr='GDP')
-        response = requests.get(DESC_URL, params=params)
-        data = response.json().decode('utf-8')
+        response = self.client.get(DESC_URL, query_string=params)
 
         assert response.status_code == 200
+
+    @pytest.mark.xfail
+    def test_get_on_valid_variable_successful_returns_valid_data(self):
+        params = dict(abbr='GDP')
+        response = self.client.get(DESC_URL, query_string=params)
+        data = json.loads(response.get_data().decode('utf-8'))
+
         assert data == {'abbr': 'GDP', 'ru': 'Цена нефти Brent', 'en': 'Brent oil price'}
 
-    # FIXME: split to two tests + rename without variable name  
     @pytest.mark.xfail
-    def test_get_rog_en_successful_returns_valid_data(self):
-        params = dict(abbr='GDP')
-        response = requests.get(DESC_URL, params=params)
-        data = response.json().decode('utf-8')
+    def test_get_on_valid_variable_successful_with_code_200(self):
+        params = dict(abbr='rog')
+        response = self.client.get(DESC_URL, query_string=params)
 
         assert response.status_code == 200
+
+    @pytest.mark.xfail
+    def test_get_on_valid_variable_successful_returns_valid_data(self):
+        params = dict(abbr='rog')
+        response = self.client.get(DESC_URL, query_string=params)
+        data = json.loads(response.get_data().decode('utf-8'))
+
         assert data == {'unit': 'rog', 'en': 'rate of growth to previous period',
                         'ru': 'темп роста к пред. периоду'}
 
-class Test_api_desc_POST_method():        
-        
+
+class Test_api_desc_POST_method(TestCaseBase):
+
     def test_post_method_without_params_fails(self):
-        response = requests.post(DESC_URL)
+        response = self.client.post(DESC_URL)
+        assert response.status_code != 200
+
+    def test_post_method_on_broken_data_returns_error_code(self):
+        response = self.client.post(DESC_URL, data="broken_data")
         assert response.status_code != 200
 
     @pytest.mark.xfail
-    def test_post_method_on_sample_payload_successful_with_code_200(self):
-        response = requests.post(DESC_URL, json=sample_post_payload)
+    def test_post_method_on_duplicate_data_fails(self):
+        upload_json = json.dumps(sample_post_payload)
+        response = self.client.post(DESC_URL, data=upload_json)
+        # second call
+        response = self.client.post(DESC_URL, data=upload_json)
+        assert response.status_code != 200
+
+    @pytest.mark.xfail
+    def test_post_method_on_new_data_successful_with_code_200(self):
+        upload_json = json.dumps(sample_post_payload)
+        response = self.client.post(DESC_URL, data=upload_json)
         assert response.status_code == 200
 
-        
-class Test_api_desc_DELETE_method():                
+    @pytest.mark.xfail
+    def test_post_method_getting_posted_successful_returns_posted_data(self):
+        upload_json = json.dumps(sample_post_payload)
+        self.client.post(DESC_URL, data=upload_json)
+
+        for desc in sample_post_payload:
+            response = self.client.get(DESC_URL, query_string={'abbr': desc.get('abbr')})
+            assert desc == json.loads(response.get_data().decode('utf-8'))
+
+
+class Test_api_desc_DELETE_method(TestCaseBase):
         
     def test_delete_method_on_empty_abbr_field_fails(self):
-        params = {'abbr': ''}
-        response = requests.delete(DESC_URL, params=params)
+        params = dict(abbr='')
+        response = self.client.delete(DESC_URL, query_string=params)
         assert response.status_code != 200
 
-        
-    # FIXME: see commented code below, it has more scenario + must rename without variable
     @pytest.mark.xfail
-    def test_delete_rog_successful_with_code_200(self):
-        params = {'abbr': 'rog'}
-        response = requests.delete(DESC_URL, params=params)
+    def test_delete_variable_desc_successful_with_code_200(self):
+        params = dict(abbr='rog')
+        response = self.client.delete(DESC_URL, query_string=params)
         assert response.status_code == 200
+
+    @pytest.mark.xfail
+    def test_delete_posted_data_successful_with_code_200(self):
+        upload_json = json.dumps(sample_post_payload)
+        self.client.post(DESC_URL, data=upload_json)
+
+        for desc in sample_post_payload:
+            response = self.client.delete(DESC_URL,
+                                          query_string={'abbr': desc.get('abbr')})
+            assert response.status_code == 200
 
 if __name__ == '__main__': # pragma no cover
     pytest.main([__file__])
@@ -111,29 +154,6 @@ if __name__ == '__main__': # pragma no cover
 # APP_URL = 'http://minikep-db.herokuapp.com'
 # API_DESC_URL = f'{APP_URL}/api/desc'
 
-
-# # GET api/desc?head=GDP
-
-# # Returns:
-# #   {'head':'GDP', 'ru':'Цена нефти Brent'}  on head
-# #   All descriptions (head and unit) without parameter
-
-# # GET api/desc?unit=rog&lang=en
-
-# # Returns:
-# #    {unit:'rog', en:'rate of growth to previous period'}
-# #    All descriptions (head and unit) without parameter
-
-# # POST api/desc
-
-# # Payload:
-# # [dict(head='BRENT', ru='Цена нефти Brent', en='Brent oil price')
-# # dict(head='GDP', ru='Валовый внутренний продукт', en='Gross domestic product')
-# # dict(unit='rog', ru='темп роста к пред. периоду', en='rate of growth to previous period')
-# # dict(unit='yoy', ru='темп роста за 12 месяцев', en='year-on-year rate of growth')]
-
-# # DELETE api/desc?unit=rog
-# # DELETE api/desc?head=BRENT
 
 # sample_post_payload = [
     # dict(head='test_BRENT', ru='Цена нефти Brent', en='Brent oil price'),
@@ -160,19 +180,17 @@ if __name__ == '__main__': # pragma no cover
         # assert response.status_code != 200
 
     # def test_getting_posted_data_is_ok(self):
-        # for desc in sample_post_payload:
-            # response = requests.get(API_DESC_URL, params={
-                # 'head': desc.get('head'),
-                # 'unit': desc.get('unit')
-            # })
-            # assert desc == response.json()
-
+    #     for desc in sample_post_payload:
+    #         response = requests.get(API_DESC_URL, params={
+    #             'head': desc.get('head'),
+    #             'unit': desc.get('unit')
+    #         })
+    #         assert desc == response.json()
+    #
     # def test_deleting_posted_data_is_ok(self):
-        # for desc in sample_post_payload:
-            # response = requests.delete(API_DESC_URL, params={
-                # 'head': desc.get('head'),
-                # 'unit': desc.get('unit')
-            # })
-# assert response.status_code == 200    
-    
-    
+    #     for desc in sample_post_payload:
+    #         response = requests.delete(API_DESC_URL, params={
+    #             'head': desc.get('head'),
+    #             'unit': desc.get('unit')
+    #         })
+    #     assert response.status_code == 200
